2a1e6826209cfb44b0791526d7260054a53d16fa
Anthony Ebert <anthonyebert@gmail.com>
2020-03-08 15:51:15 +0100

    protezione civile download, recovered and deaths

--- protezione-civile-download.R
+++ protezione-civile-download.R
@@ -5,14 +5,40 @@ library(dplyr)
 x = readr::read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv")
 x$data <- lubridate::as_date(x$data)
 
-x <- x %>%
+# Confirmed
+
+x_confirmed <- x %>%
   select(data, denominazione_regione, totale_casi) %>%
   tidyr::pivot_wider(
-    names_from = denominazione_regione, 
+    names_from = denominazione_regione,
     values_from = totale_casi
   ) %>%
   rename(date = data)
 
-readr::write_excel_csv(x, path = "protezione-civile-download/transition_matrix_regione_confirmed.csv")
+readr::write_excel_csv(x_confirmed, path = "protezione-civile-download/transition_matrix_regione_confirmed.csv")
+
+# Deaths
+
+x_deaths <- x %>%
+  select(data, denominazione_regione, deceduti) %>%
+  tidyr::pivot_wider(
+    names_from = denominazione_regione,
+    values_from = deceduti
+  ) %>%
+  rename(date = data)
+
+readr::write_excel_csv(x_confirmed, path = "protezione-civile-download/transition_matrix_regione_deaths.csv")
+
+# Recovered
+
+x_recovered <- x %>%
+  select(data, denominazione_regione, dimessi_guariti) %>%
+  tidyr::pivot_wider(
+    names_from = denominazione_regione,
+    values_from = dimessi_guariti
+  ) %>%
+  rename(date = data)
+
+readr::write_excel_csv(x_confirmed, path = "protezione-civile-download/transition_matrix_regione_recovered.csv")
 
 

a3d0ccdbf40a7142d2edb65e65e56f6532565a10
Anthony Ebert <anthonyebert@gmail.com>
2020-03-08 17:35:38 +0100

    make sure factor levels are correct.

--- adjacency_matricies_provinces.R
+++ adjacency_matricies_provinces.R
@@ -1,6 +1,22 @@
 # Adjacency matrix provinces
 
-z = readxl::read_xlsx("Francesco/Adj matrix PROVINCE ITALIA_1.xlsx", skip = 1) %>%
-  select(-Regione)
+x = readr::read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv")
 
-readr::write_excel_csv(z, path = "protezione-civile-download/adjacency_matrix_provinces_3-weights.csv")
+z = readxl::read_xlsx("Francesco/Adj matrix PROVINCE ITALIA_1.xlsx", skip = 1)
+
+# Change factor levels to those used by the Italian government.
+
+z$Provincia <- factor(z$Provincia) %>%
+  forcats::fct_recode(
+    `Ascoli Piceno` = "Ascoli-Piceno",
+    `Forlì-Cesena` = "Forli-Cesena",
+    `La Spezia` = "La-Spezia",
+    `Massa Carrara` = "Massa-Carrara",
+    `Monza e della Brianza` = "Monza-Brianza",
+    `Pesaro e Urbino` = "Pesaro-Urbino",
+    `Reggio di Calabria` = "Reggio-Calabria",
+    `Reggio nell'Emilia` = "Reggio-Emilia",
+    `Vibo Valentia` = "Vibo-Valentia"
+  )
+
+readr::write_excel_csv(z[,-1], path = "protezione-civile-download/adjacency_matrix_provinces_3-weights.csv")

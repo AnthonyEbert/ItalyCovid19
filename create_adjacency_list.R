f8105fc25c4f319d64541a75954ad0902965c0d8
AnthonyEbert <anthonyebert@gmail.com>
2020-03-09 00:16:57 +0100

    francesco_list recoded to Johns Hopkins

--- create_adjacency_list.R
+++ create_adjacency_list.R
@@ -12,10 +12,27 @@ francesco_list <- francesco_mat %>%
   Matrix::Matrix(sparse = TRUE) %>%
   summary()
 
-francesco_list[,1] <- levels(factor(y$...3))[francesco_list[,1]]
-francesco_list[,2] <- levels(factor(y$...3))[francesco_list[,2]]
+country_names <- factor(colnames(francesco_mat))
+country_names <- forcats::fct_recode(
+  country_names, 
+  Australia = "Australia (Diamond Princess)",
+  `Bosnia and Herzegovina` = "Bosnia ed Erzegovina",
+  Chad = "Ciad",
+  Israel = "Israel (Diamond Princess)",
+  Kazakhstan = "Kazakistan",
+  Macau = "Macao",
+  `Mainland China` = "China",
+  `Netherlands` = "Netherland",
+  `New Zealand` = "New Zeland",
+  `Peru` = "Perù",
+  `United Arab Emirates` = "United  Arab Emirates",
+  `US` = "USA"
+)
+
+francesco_list[,1] <- country_names[francesco_list[,1]]
+francesco_list[,2] <- country_names[francesco_list[,2]]
 francesco_list <- francesco_list[,c(2,1,3)]
 
-xlsx::write.xlsx(francesco_list, "Francesco/francesco_list.xlsx")
+readr::write_csv(francesco_list, "Francesco/francesco_list.csv")
 
 

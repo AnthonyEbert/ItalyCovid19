fa72874fed870cde938d5c9c56f3c7956b2fe80e
AnthonyEbert <anthonyebert@gmail.com>
2020-03-09 16:12:56 +0100

    transition matrix regions lat-long-weight

---
+++ adjacency_matricies_regione_lat-long-weight.R
@@ -0,0 +1,20 @@
+# Create adjacency matrix for countries
+
+library(dplyr)
+
+## Johns Hopkins data -------------
+
+x = read.csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv")
+
+## Get average lat and long for all countries ----------
+
+x1 <- x %>%
+  group_by(denominazione_regione) %>%
+  summarise(Long = mean(long), Lat = mean(lat))
+
+# Create adjacency matrix
+
+kol = ItalyCovid19::create_adjacency_matrix(x1$denominazione_regione, x1$Lat, x1$Long)
+diag(kol) = max(as.numeric(kol[is.finite(as.numeric(kol))])) + 1
+
+write.csv(kol, "protezione-civile-download/adjacency_matrix_regione_lat-long-weight.csv")

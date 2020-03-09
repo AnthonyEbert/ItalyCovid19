9a8af4a05223f4848c25fee0d8dc08b1e3acdcf6
AnthonyEbert <anthonyebert@gmail.com>
2020-03-09 14:33:35 +0100

    replace diagonal zero with max.

--- adjacency_matricies_countries_lat-long-weight.R
+++ adjacency_matricies_countries_lat-long-weight.R
@@ -15,6 +15,6 @@ x1 <- x %>%
 # Create adjacency matrix
 
 kol = ItalyCovid19::create_adjacency_matrix(x1$Country.Region, x1$Lat, x1$Long)
-diag(kol) = 0
+diag(kol) = max(as.numeric(kol[is.finite(as.numeric(kol))])) + 1
 
 write.csv(kol, "johns-hopkins-download/adjacency_matrix_countries_lat-long-weight.csv")

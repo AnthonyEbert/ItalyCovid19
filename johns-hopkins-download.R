5417704b3652b247b716f683b2c3b4e470a92a74
AnthonyEbert <anthonyebert@gmail.com>
2020-03-13 13:12:08 +0100

    Johns Hopkins to alpha3 ISO3166

--- johns-hopkins-download.R
+++ johns-hopkins-download.R
@@ -8,10 +8,10 @@ x = read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/c
 
 iso3166 <- read.csv("https://raw.githubusercontent.com/AnthonyEbert/COVID-19_ISO-3166/master/JohnsHopkins-to-A3.csv")
 
-# x <- x %>%
-#   left_join(iso3166) %>%
-#   mutate(Country.Region = alpha3) %>%
-#   select(-alpha3)
+x <- x %>%
+  left_join(iso3166) %>%
+  mutate(Country.Region = alpha3) %>%
+  select(-alpha3)
 
 x = x %>%
   group_by(Country.Region) %>%
@@ -34,6 +34,11 @@ readr::write_excel_csv(x, path = "johns-hopkins-download/transition_matrix_count
 
 x = read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
 
+x <- x %>%
+  left_join(iso3166) %>%
+  mutate(Country.Region = alpha3) %>%
+  select(-alpha3)
+
 x = x %>%
   group_by(Country.Region) %>%
   summarise_at(names(.)[purrr::partial(startsWith, prefix = "X")(names(.))], sum) %>%
@@ -51,9 +56,14 @@ x <- x %>%
 
 readr::write_excel_csv(x, path = "johns-hopkins-download/transition_matrix_countries_deaths.csv")
 
-## Deaths
+## Recovered
 
 x = read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
+
+x <- x %>%
+  left_join(iso3166) %>%
+  mutate(Country.Region = alpha3) %>%
+  select(-alpha3)
 
 x = x %>%
   group_by(Country.Region) %>%
